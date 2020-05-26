a039daed75e8efa8adeee76a094cd6fa7ba91f5d
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2020-05-26 12:38:55 +0300

    Clean up documentation generation
    * Single `main`
    * Text reflow
    
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- doc/generate.py
+++ doc/generate.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 from os.path import join, dirname
-try:
-    from robot.libdoc import libdoc
-except:
-    def main():
-        print """Robot Framework 2.7 or later required for generating documentation"""
-else:
-    def main():
-        libdoc(join(dirname(__file__), '..', 'src', 'BrowserMobProxyLibrary'), join(dirname(__file__), 'BrowserMobProxyLibrary.html'))
+
+
+def main():
+    try:
+        from robot.libdoc import libdoc
+    except Exception as ex:
+        err_msg = "Robot Framework 2.7 or later required for generating documentation"
+        raise RuntimeError(err_msg) from ex
+
+    libdoc(join(dirname(__file__), '..', 'src', 'BrowserMobProxyLibrary'),
+           join(dirname(__file__), 'BrowserMobProxyLibrary.html'))
 
 
 if __name__ == '__main__':
